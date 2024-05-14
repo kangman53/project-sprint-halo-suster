25a91eb3b4bf47d3906b83af09582053689cad24
Herman Yulianto <herman@delman.io>
2024-05-14 17:43:26 +0700

    change api response

--- service/file/file_service_impl.go
+++ service/file/file_service_impl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	file_entity "github.com/kangman53/project-sprint-halo-suster/entity/file"
 	exc "github.com/kangman53/project-sprint-halo-suster/exceptions"
 	"github.com/kangman53/project-sprint-halo-suster/helpers"
 )
@@ -18,28 +19,32 @@ func NewFileService() FileService {
 	return &fileServiceImpl{}
 }
 
-func (service *fileServiceImpl) Upload(ctx *fiber.Ctx) (string, error) {
+func (service *fileServiceImpl) Upload(ctx *fiber.Ctx) (file_entity.UploadImageResponse, error) {
 	file, err := ctx.FormFile("file")
 	if err != nil {
-		return "", err
+		return file_entity.UploadImageResponse{}, err
 	}
 	allowedTypes := []string{"image/jpeg", "image/jpg"}
 	if !slices.Contains(allowedTypes, file.Header.Get("Content-Type")) {
 		// Handle invalid file type
-		return "", exc.BadRequestException("Invalid File Type")
+		return file_entity.UploadImageResponse{}, exc.BadRequestException("Invalid File Type")
 	}
 
 	MIN_SIZE, MAX_SIZE := 10*1024, 2*1024*1024
 	if file.Size < int64(MIN_SIZE) || file.Size > int64(MAX_SIZE) {
 		// Handle invalid file size
-		return "", exc.BadRequestException("Invalid File Size")
+		return file_entity.UploadImageResponse{}, exc.BadRequestException("Invalid File Size")
 	}
 
 	uniqueId := uuid.New().String()
 	extensionName := strings.Split(file.Header.Get("Content-Type"), "/")[1]
 	fileName := fmt.Sprintf("%s.%s", uniqueId, extensionName)
-	if err := helpers.FileUpload(file, fileName); err != nil {
-		return "", err
+	url, err := helpers.FileUpload(file, fileName)
+	if err != nil {
+		return file_entity.UploadImageResponse{}, err
 	}
-	return "Successfully upload image", nil
+	return file_entity.UploadImageResponse{
+		Message: "Successfully upload image",
+		Data:    map[string]string{"imageUrl": url},
+	}, nil
 }
