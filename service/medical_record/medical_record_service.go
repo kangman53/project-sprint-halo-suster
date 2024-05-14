4a4b2de7b937164f8b731fb0bc1892ff50901682
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-14 18:08:47 +0700

    add create medical record service

--- service/medical_record/medical_record_service.go
+++ service/medical_record/medical_record_service.go
@@ -3,10 +3,12 @@ package medical_record_service
 import (
 	"context"
 
+	"github.com/gofiber/fiber/v2"
 	medical_record_entity "github.com/kangman53/project-sprint-halo-suster/entity/medical_record"
 )
 
 type MedicalRecordService interface {
 	CreatePatient(ctx context.Context, req medical_record_entity.CreateMRPatientRequest) (medical_record_entity.CreateMRPatientResponse, error)
 	SearchPatient(ctx context.Context, query medical_record_entity.SearchMRPatientQuery) (medical_record_entity.SearchMRPatientResponse, error)
+	CreateMedicalRecord(ctx *fiber.Ctx, req medical_record_entity.CreateMedicalRecordRequest) (medical_record_entity.CreateMedicalRecordResponse, error)
 }
--- service/medical_record/medical_record_service_impl.go
+++ service/medical_record/medical_record_service_impl.go
@@ -6,19 +6,23 @@ import (
 	"strings"
 
 	"github.com/go-playground/validator"
+	"github.com/gofiber/fiber/v2"
 	medical_record_entity "github.com/kangman53/project-sprint-halo-suster/entity/medical_record"
 	exc "github.com/kangman53/project-sprint-halo-suster/exceptions"
 	medical_record_repository "github.com/kangman53/project-sprint-halo-suster/repository/medical_record"
+	authService "github.com/kangman53/project-sprint-halo-suster/service/auth"
 )
 
 type medicalRecordServiceImpl struct {
 	MedicalRecordRepository medical_record_repository.MedicalRecordRepository
+	AuthService             authService.AuthService
 	Validator               *validator.Validate
 }
 
-func NewMedicalRecordService(medicalRecordRepostory medical_record_repository.MedicalRecordRepository, validator *validator.Validate) MedicalRecordService {
+func NewMedicalRecordService(medicalRecordRepostory medical_record_repository.MedicalRecordRepository, authService authService.AuthService, validator *validator.Validate) MedicalRecordService {
 	return &medicalRecordServiceImpl{
 		MedicalRecordRepository: medicalRecordRepostory,
+		AuthService:             authService,
 		Validator:               validator,
 	}
 }
@@ -65,5 +69,35 @@ func (service *medicalRecordServiceImpl) SearchPatient(ctx context.Context, sear
 	return medical_record_entity.SearchMRPatientResponse{
 		Message: "success",
 		Data:    patientSearched,
+	}, nil
+}
+
+func (service *medicalRecordServiceImpl) CreateMedicalRecord(ctx *fiber.Ctx, req medical_record_entity.CreateMedicalRecordRequest) (medical_record_entity.CreateMedicalRecordResponse, error) {
+	if err := service.Validator.Struct(req); err != nil {
+		return medical_record_entity.CreateMedicalRecordResponse{}, exc.BadRequestException(fmt.Sprintf("Bad request: %s", err))
+	}
+
+	userId, err := service.AuthService.GetValidUser(ctx)
+	if err != nil {
+		return medical_record_entity.CreateMedicalRecordResponse{}, exc.UnauthorizedException("Unauthorized")
+	}
+
+	medicalRecord := medical_record_entity.MedicalRecord{
+		IdentityNumber: req.IdentityNumber,
+		Symptoms:       req.Symptoms,
+		Medications:    req.Medications,
+		CreateBy:       userId,
+	}
+	medicalRecordCreated, err := service.MedicalRecordRepository.CreateMedicalRecord(ctx.UserContext(), medicalRecord)
+	if err != nil {
+		if strings.Contains(err.Error(), "no rows in result set") {
+			return medical_record_entity.CreateMedicalRecordResponse{}, exc.BadRequestException("identityNumber is not exist")
+		}
+		return medical_record_entity.CreateMedicalRecordResponse{}, err
+	}
+
+	return medical_record_entity.CreateMedicalRecordResponse{
+		Message: "Medical Record successfully created",
+		Data:    &medicalRecordCreated,
 	}, nil
 }
