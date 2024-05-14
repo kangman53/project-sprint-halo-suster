ffa69357b15eeebc5cc6a3d3ee0da0ebfc31d2ea
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-14 18:06:40 +0700

    add medical records table

--- database/initiate_tables.go
+++ database/initiate_tables.go
@@ -54,6 +54,18 @@ func InitiateTables(dbPool *pgxpool.Pool) error {
 		CREATE INDEX IF NOT EXISTS patients_created_at_asc
 			ON patients (created_at ASC);
 		`,
+		`
+		CREATE TABLE IF NOT EXISTS medical_records (
+			id VARCHAR(100) PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+			patient_identity_number VARCHAR(20) NOT NULL,
+			symptoms TEXT NOT NULL,
+            medicatons TEXT NOT NULL,
+			created_by VARCHAR(100) NOT NULL,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (patient_identity_number) REFERENCES patients(identity_number) ON DELETE NO ACTION,
+			FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE NO ACTION
+		);
+		`,
 		// Add more table creation queries here if needed
 	}
 
