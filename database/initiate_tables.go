d63c3e1717c8fe2fe76acfe72974187c3823f0f7
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-14 20:04:52 +0700

    update column name medical_record

--- database/initiate_tables.go
+++ database/initiate_tables.go
@@ -59,7 +59,7 @@ func InitiateTables(dbPool *pgxpool.Pool) error {
 			id VARCHAR(100) PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
 			patient_identity_number VARCHAR(20) NOT NULL,
 			symptoms TEXT NOT NULL,
-            medicatons TEXT NOT NULL,
+            medications TEXT NOT NULL,
 			created_by VARCHAR(100) NOT NULL,
 			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			FOREIGN KEY (patient_identity_number) REFERENCES patients(identity_number) ON DELETE NO ACTION,
